3b5760591cdfbc8802ce4ccb03893e24aa8553ec
litianpeng <qq52111314>
2020-12-14 11:36:02 +0800

    新增允许加入群聊最大人数,群描述,加群类型(自动加入or审核加入)属性

--- ruoyi-admin/src/main/java/com/ruoyi/im/domain/im/GroupChat.java
+++ ruoyi-admin/src/main/java/com/ruoyi/im/domain/im/GroupChat.java
@@ -25,4 +25,18 @@ public class GroupChat {
      * 创建时间
      */
     private String createTime;
+    /**
+     * 群描述
+     */
+    private String remark;
+
+    /**
+     * 加群类型 1自动加入 0审核加入（默认方式）
+     */
+    private Integer type;
+
+    /**
+     * 允许加入群聊最大人数
+     */
+    private Integer maxPerson;
 }
